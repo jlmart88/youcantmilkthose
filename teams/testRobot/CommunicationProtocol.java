4a82ed059b611baedd3091e67e2566cb8ad4a97b
Kevin Wen <kevinwen94@gmail.com>
2014-01-09 06:35:44 -0500

    combined code to testRobot

---
+++ teams/testRobot/CommunicationProtocol.java
@@ -0,0 +1,89 @@
+package testRobot;
+
+/**
+ * This class provides the definitions for how robots will broadcast and read to the
+ * global channels
+ * 
+ * @author jlmart88
+ *
+ */
+public class CommunicationProtocol {
+	//channel definitions
+	
+	//the default spacer to use between data on broadcasts
+	public static final int DEFAULT_SPACER = 101;
+	
+	/**roleChannels display what role each robot should play
+	 * 
+	 * exist on the range 100-124 
+	 * format for channel data: (robotID)(defaultSpacer)(robotRole)
+	 */
+	public static final int ROLE_CHANNEL_MIN = 100;
+	public static final int ROLE_CHANNEL_MAX = 124;
+	public static final int ROLE_CHANNELS[] = range(ROLE_CHANNEL_MIN,ROLE_CHANNEL_MAX+1);
+	public static final int ROLE_CHANNEL_NUM = ROLE_CHANNELS.length;	
+	
+	/** Takes in the data stored in a channel and returns what role the channel data is displaying
+	 * 
+	 * If the channelData is not properly formatted for displaying a role, it will default to returning
+	 * RobotRole.CONSTRUCTOR
+	 * 
+	 * @param channelData the integer stored in the data
+	 * @return RobotRole the role the channel is displaying
+	 */
+	public static RobotRole dataToRole(int channelData){
+		//take the last digit of the data, since roles are only one digit
+		int communicationID = channelData%10;
+		for (RobotRole role: RobotRole.values()){
+			if (role.communicationID == communicationID){
+				return role;
+			}
+		}
+		// if we get here, then the channel data was improperly formatted,
+		// so default to returning the Constructor role
+		return RobotRole.CONSTRUCTOR;
+	}
+	
+	/** Takes in the data stored in a channel and returns what robotID the channel data is displaying
+	 * 
+	 * If the channelData is not properly formatted for displaying the robotID, then will default to returning
+	 * -1
+	 * 
+	 * BYTECODE COST: 20
+	 * 
+	 * @param channelData the integer stored in the data
+	 * @return RobotRole the role the channel is displaying
+	 */
+	public static int dataToRobotID(int channelData){
+		String dataString = Integer.toString(channelData);
+		int spacerLocation = dataString.lastIndexOf(Integer.toString(DEFAULT_SPACER));
+		if (spacerLocation != -1){
+			return Integer.parseInt(dataString.substring(0, spacerLocation));
+		}
+		// if we get here, then the channel data was improperly formatted,
+		// so default to returning -1
+		return -1;
+	}
+	
+	/** Takes in a robotID and a robotRole and returns the corresponding channelData
+	 * 
+	 * 
+	 * @param robotID integer of the robot's id
+	 * @param role RobotRole the robot should have
+	 * @return int the channel data to broadcast
+	 */
+	public static int roleToData(int robotID, RobotRole role){
+		return Integer.parseInt(Integer.toString(robotID)+
+				Integer.toString(DEFAULT_SPACER)+Integer.toString(role.communicationID));	
+	}
+	
+	//returns an int[] array where 'begin' is inclusive, e'nd' is exclusive
+	//ex: range(3,6) ---> [3,4,5]
+	private static int[] range(int begin, int end){
+		int out[] = new int[end-begin];
+		for (int i=0;i<end-begin;i++){
+			out[i]=begin+i;
+		}
+		return out;
+	}
+}
---
+++ teams/testRobot/RobotPlayer.java
@@ -0,0 +1,218 @@
+package testRobot;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class RobotPlayer{
+    
+    public static RobotController rc;
+    static Direction allDirections[] = Direction.values();
+    static Random randall = new Random();
+    static int directionalLooks[] = new int[]{0,1,-1,2,-2};
+    static boolean mapCreated = false;
+    
+	//constants for assigning roles
+	static RobotRole myRole = null;
+	static int currentRoleChannel = 0;
+	static int lastSpawnedID = 0;
+	static boolean justSpawned = false;
+
+    
+    public static void run(RobotController rcin){
+        rc = rcin;
+        randall.setSeed(rc.getRobot().getID());
+        int height = rc.getMapHeight();
+        int width = rc.getMapWidth();
+        while(true){
+            try{
+                if(rc.getType()==RobotType.HQ){//if I'm a headquarters
+                    runHeadquarters(height,width);
+                }else if(rc.getType()==RobotType.SOLDIER){
+                    runSoldier(height, width);
+                }
+                rc.yield();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void runSoldier(int height, int width) throws GameActionException {
+    	
+		//if we dont have a role, sift through the roleChannels to try to find one
+		if (myRole == null){
+			for (int channel:CommunicationProtocol.ROLE_CHANNELS){
+				int channelData = rc.readBroadcast(channel);
+				if (CommunicationProtocol.dataToRobotID(channelData)==rc.getRobot().getID()){
+					myRole = CommunicationProtocol.dataToRole(channelData);
+					rc.setIndicatorString(1, "My role is: "+myRole.name());
+				}	
+			}
+		}
+
+        tryToShoot();
+        //movement
+        Direction chosenDirection = allDirections[(int)(randall.nextDouble()*8)];
+//        if(rc.isActive()&&rc.canMove(chosenDirection)){
+//            Robot[] nearbyRobots = rc.senseNearbyGameObjects(Robot.class, 5);
+//            RobotInfo x;
+//            for(Robot robot:nearbyRobots){
+//                x = rc.senseRobotInfo(robot);
+//                if(x.isConstructing){
+//                    rc.sneak(chosenDirection);
+//                }
+//            }
+//            rc.move(chosenDirection);
+//        }
+        swarmMove(height, width);
+    }
+    
+    private static void swarmMove(int height, int width) throws GameActionException{
+        Direction chosenDirection = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+        if(rc.isActive()){
+            if(randall.nextDouble()<0.5){//go to swarm center
+                for(int directionalOffset:directionalLooks){
+                    int forwardInt = chosenDirection.ordinal();
+                    Direction trialDir = allDirections[(forwardInt+directionalOffset+8)%8];
+                    if(rc.canMove(trialDir)){
+                        rc.move(trialDir);
+                        break;
+                    }
+                }
+            }else{//go wherever the wind takes you
+                Direction d = allDirections[(int)(randall.nextDouble()*8)];
+                if(rc.isActive()&&rc.canMove(d)){
+                    rc.move(d);
+                }
+            }
+        }
+    }
+    
+    private static MapLocation mladd(MapLocation m1, MapLocation m2){
+        return new MapLocation(m1.x+m2.x,m1.y+m2.y);
+    }
+    
+    private static MapLocation mldivide(MapLocation bigM, int divisor){
+        return new MapLocation(bigM.x/divisor, bigM.y/divisor);
+    }
+
+    private static int locToInt(MapLocation m){
+        return (m.x*100 + m.y);
+    }
+    
+    private static MapLocation intToLoc(int i){
+        return new MapLocation(i/100,i%100);
+    }
+    
+    private static int senseCowsAtRange(MapLocation loc) throws GameActionException{
+        int cows = 0;
+        MapLocation[] adjLocs = MapLocation.getAllMapLocationsWithinRadiusSq(loc, 5);
+        for(MapLocation innerLoc: adjLocs){
+            cows += rc.senseCowsAtLocation(innerLoc);
+        }
+        return cows;
+    }
+
+    private static void tryToShoot() throws GameActionException {
+        //shooting
+        Robot[] enemyRobots = rc.senseNearbyGameObjects(Robot.class,10000,rc.getTeam().opponent());
+        if(enemyRobots.length>0){//if there are enemies
+            Robot anEnemy = enemyRobots[0];
+            RobotInfo anEnemyInfo;
+            anEnemyInfo = rc.senseRobotInfo(anEnemy);
+            if(anEnemyInfo.type == RobotType.HQ && enemyRobots.length>1){
+                anEnemy = enemyRobots[1];
+                anEnemyInfo = rc.senseRobotInfo(anEnemy);
+                if(anEnemyInfo.location.distanceSquaredTo(rc.getLocation())<rc.getType().attackRadiusMaxSquared){
+                    if(rc.isActive()){
+                        rc.attackSquare(anEnemyInfo.location);
+                    }
+                }
+            }
+            else if(anEnemyInfo.type != RobotType.HQ){
+                if(anEnemyInfo.location.distanceSquaredTo(rc.getLocation())<rc.getType().attackRadiusMaxSquared){
+                    if(rc.isActive()){
+                        rc.attackSquare(anEnemyInfo.location);
+                    }
+                }
+            }
+        }else{//there are no enemies, so build a tower
+            if(randall.nextDouble()<1 && rc.sensePastrLocations(rc.getTeam()).length<10){
+                if (senseCowsAtRange(rc.getLocation()) > 2500){
+                    if(rc.isActive()){
+                        rc.construct(RobotType.PASTR);
+//                        System.out.println((senseCowsAtRange(rc.getLocation())));
+                    }
+                }
+            }
+        }
+    }
+
+    
+    private static void runHeadquarters(int height, int width) throws GameActionException {
+    	
+		//give myself the role of HQ
+		if (myRole == null){
+			myRole = RobotRole.HQ;
+		}
+		
+		Direction spawnDir = Direction.NORTH;
+		if(rc.isActive()&&rc.canMove(spawnDir)&&rc.senseRobotCount()<GameConstants.MAX_ROBOTS){
+			rc.spawn(Direction.NORTH);
+			justSpawned = true;//tell the HQ to try to broadcast role information
+		}
+		
+		if(justSpawned){//try to find our most recent spawn, and broadcast its role info
+			Robot[] nearbyRobots = rc.senseNearbyGameObjects(Robot.class, 9, rc.getTeam());
+			for (Robot spawnedRobot:nearbyRobots){
+				int spawnedID = spawnedRobot.getID();
+				if(spawnedID>lastSpawnedID){//our next spawn will have a greater id than the last spawn
+					//for now, randomly choose a role
+					RobotRole role = RobotRole.values()[(int)(randall.nextDouble()*10)%4];
+					
+					rc.broadcast(CommunicationProtocol.ROLE_CHANNELS[currentRoleChannel], 
+								CommunicationProtocol.roleToData(spawnedRobot.getID(), role));
+					
+					justSpawned = false; //reset the trigger for broadcasting roles
+					
+					//increment/loop role channel
+					currentRoleChannel = (currentRoleChannel+1)%CommunicationProtocol.ROLE_CHANNEL_NUM; 
+					
+					lastSpawnedID=spawnedID;//track the most recent spawn
+					
+					rc.setIndicatorString(0, "Just Spawned a "+role.name()+" with ID: "+lastSpawnedID);
+				}
+			}
+		}
+        char[][] map = new char[width][height];
+        if(rc.isActive()&&rc.canMove(spawnDir)&&rc.senseRobotCount()<GameConstants.MAX_ROBOTS){
+            rc.spawn(Direction.NORTH);
+        }
+        if(mapCreated == false){ //Create a map of the battlefield
+            for(int y=0; y<height; y++){
+                for(int x=0; x<width; x++){
+                    if(rc.senseTerrainTile(new MapLocation(x,y)) == TerrainTile.NORMAL){
+                        map[x][y] = '-';
+                    }
+                    else if(rc.senseTerrainTile(new MapLocation(x,y)) == TerrainTile.VOID){
+                        map[x][y] = '#';
+                    }
+                    else if(rc.senseTerrainTile(new MapLocation(x,y)) == TerrainTile.OFF_MAP){
+                        map[x][y] = '^';
+                    }
+                    else if(rc.senseTerrainTile(new MapLocation(x,y)) == TerrainTile.ROAD){
+                        map[x][y] = '=';
+                    }
+                    System.out.print(map[x][y]);
+                }
+                System.out.println();
+            }
+            mapCreated = true;
+        }
+//        int editingChannel = (Clock.getRoundNum()%2);
+//        int usingChannel = ((Clock.getRoundNum()+1)%2);
+//        rc.broadcast(editingChannel, 0);
+//        rc.broadcast(editingChannel+2, 0);
+    }
+}
